b82747ea054e543e20c770aa2e2a415b1a9eb8f2
intelligentchild <shiv.kurtartkar@gmail.com>
2020-10-18 20:25:32 +0530

    dev: accepting cmd options

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <algorithm>
+
+char* getCmdOption(char **begin, char **end, const std::string& option){
+	char** iter = std::find(begin, end, option);
+	if(iter != end && ++iter != end){
+		return *iter;
+	}
+	return 0;
+}
+
+bool getCmdOptionExists(char **begin, char **end, const std::string& option){
+	return std::find(begin, end, option) != end;
+}
 
 int main(int argc, char** argv){
 
-	
+	if(getCmdOptionExists(argv, argv+argc, "-h")){
+		std::cout<<"HELP -- is not implemented yet \n";
+		// TODO: print help
+		return 0;
+	}
 
-	std::cout<<"Working";
+	char* filename = getCmdOption(argv, argv+argc, "-f");
+	if(filename){
+		std::cout<<" filename: "<<filename<<std::endl;
+	}else{
+		std::cout<<"please specify filename using '-f <filename>' \n";
+	}
+	
+	return 0;
+
 }
